3dcae2d41698b60ba0d52f99838a9f59dc01f7c5
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-02-10 23:09:04 -0500

    builtins

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,28 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.print("$ ");
-
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        
-        do {
-            if (input.equals("exit 0"))
+        String input, typeSubstring;
+        String[] commands = { "echo", "exit", "type" };
+        while (true) {
+            System.out.print("$ ");
+            input = scanner.nextLine();
+            if (input.equals("exit 0")) {
                 break;
-
-            if(input.startsWith("echo")) {
-                System.out.println(input.substring(5, input.length()));
+            } else if (input.startsWith("echo")) {
+                System.out.println(input.substring(5));
+            } else if (input.startsWith("type")) {
+                typeSubstring = input.substring(5);
+                if (Arrays.asList(commands).contains(typeSubstring)) {
+                    System.out.println(typeSubstring + " is a shell builtin");
+                } else {
+                    System.out.println(typeSubstring + " not found");
+                }
             } else {
                 System.out.println(input + ": command not found");
             }
-            System.out.print("$ ");
-            input = scanner.nextLine();
-        } while (!input.matches(""));
-        
-        scanner.close();
+        }
     }
 }
