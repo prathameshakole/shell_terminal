c258c679e48a6e3c4af49654d40a4b13ba26fcce
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-02-11 09:36:31 -0500

    PATH

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,55 +4,62 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        String typeSubstring;
-        String[] commands = { "echo", "exit", "type" };
-        String[] str = input.split(" ");
+    private static final String[] BUILTIN_COMMANDS = { "echo", "exit", "type" };
 
-        String parameter = "";
-        if (str.length > 2) {
-            for (int i = 1; i < str.length; i++) {
-            if (i < str.length - 1) {
-                parameter += str[i] + " ";
-            } else {
-                parameter += str[i];
-            }
-            }
-        } else if (str.length > 1) {
-            parameter = str[1];
-        }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         while (true) {
             System.out.print("$ ");
-            input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             if (input.equals("exit 0")) {
                 break;
-            } else if (input.startsWith("echo")) {
-                System.out.println(input.substring(5));
-            } else if (input.startsWith("type")) {
-                typeSubstring = input.substring(5);
-                if (Arrays.asList(commands).contains(typeSubstring)) {
-                    System.out.println(typeSubstring + " is a shell builtin");
-                } else {
-                    String path = getPath(parameter);
-                    if (path != null) {
-                        System.out.println(parameter + " is " + path);
-                    } else {
-                        System.out.println(parameter + ": not found");
-                    }
-                }
-            } else {
-                System.out.println(input + ": command not found");
             }
+
+            String[] str = input.split(" ", 2);
+            String command = str[0];
+            String parameter = (str.length > 1) ? str[1] : "";
+
+            if (command.equals("echo")) {
+                System.out.println(parameter);
+            } else if (command.equals("type")) {
+                handleTypeCommand(parameter);
+            } else {
+                System.out.println(command + ": command not found");
+            }
+        }
+        scanner.close();
+    }
+
+    private static void handleTypeCommand(String parameter) {
+        if (parameter.isEmpty()) {
+            System.out.println("type: missing operand");
+            return;
+        }
+
+        if (Arrays.asList(BUILTIN_COMMANDS).contains(parameter)) {
+            System.out.println(parameter + " is a shell builtin");
+            return;
+        }
+
+    
+        String executablePath = findExecutableInPath(parameter);
+        if (executablePath != null) {
+            System.out.println(parameter + " is " + executablePath);
+        } else {
+            System.out.println(parameter + ": not found");
         }
     }
 
-    private static String getPath(String parameter) {
-        for (String path : System.getenv("PATH").split(";")) {
-            Path fullPath = Path.of(path, parameter);
-            if (Files.isRegularFile(fullPath)) {
+    private static String findExecutableInPath(String command) {
+        String pathEnv = System.getenv("PATH");
+        if (pathEnv == null || pathEnv.isEmpty()) {
+            return null;
+        }
+
+        for (String dir : pathEnv.split(":")) {  
+            Path fullPath = Path.of(dir, command);
+            if (Files.isRegularFile(fullPath) && Files.isExecutable(fullPath)) {
                 return fullPath.toString();
             }
         }
