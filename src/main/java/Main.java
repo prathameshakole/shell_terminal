a1bb27fc6f75b63454261e6b07779606c414224c
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-02-10 22:53:19 -0500

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
         System.out.print("$ ");
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-        
-        System.out.println(input + ": command not found");
-        
-        
+        do {
+            System.out.println(input + ": command not found");
+            System.out.print("$ ");
+            input = scanner.nextLine();
+          } while (!input.matches(""));
+        scanner.close();
     }
 }
