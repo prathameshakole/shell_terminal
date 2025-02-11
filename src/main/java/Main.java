ba083c69e56bf99ea90936c6fa41f8c25dd352d2
Prathamesh Akole <akoleprathamesh@gmail.com>
2025-02-10 23:01:42 -0500

    IZ3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,8 +13,9 @@ public class Main {
 
             if(input.startsWith("echo")) {
                 System.out.println(input.substring(5, input.length()));
+            } else {
+                System.out.println(input + ": command not found");
             }
-            System.out.println(input + ": command not found");
             System.out.print("$ ");
             input = scanner.nextLine();
         } while (!input.matches(""));
